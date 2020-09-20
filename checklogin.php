7c5c6228d5d6760a016d2817c0948a46de5913a6
Anjushabj <anjushabj@gmail.com>
2020-09-21 05:24:57 +0530

    Add php codes for login

---
+++ checklogin.php
@@ -0,0 +1,138 @@
+<?php
+
+session_start();
+
+require_once("db.php");
+
+//If user Actually clicked login button 
+if(isset($_POST)) {
+
+	//Escape Special Characters in String
+	$email = mysqli_real_escape_string($conn, $_POST['email']);
+	$password = mysqli_real_escape_string($conn, $_POST['password']);
+	
+	//Encrypt Password
+	$password = base64_encode(strrev(md5($password)));
+
+	//sql query to check user login
+	$sql2 = "SELECT loginid,email,password FROM login WHERE email='$email' AND password='$password'";
+	
+	$user = "SELECT id_user, firstname, lastname, email, active FROM users WHERE email='$email'";
+	$company = "SELECT id_company, companyname, email, active FROM company WHERE email='$email'";
+
+	$role = "SELECT role FROM login WHERE email='$email' AND password='$password'";
+	
+    $result = $conn->query($role);
+    $value = $result->fetch_assoc();
+	
+    if($value["role"] == "user")
+	{
+	$result = "";
+	if($conn->query($sql2)->num_rows > 0)
+	{
+	$result = $conn->query($user);
+	}
+	//if user table has this this login details
+	if($result->num_rows > 0) {
+		while($row = $result->fetch_assoc()) {
+
+			if($row['active'] == '0') {
+				$_SESSION['loginActiveError'] = "Your Account Is Not Active. Check Your Email.";
+		 		header("Location: login.php");
+				exit();
+			} else if($row['active'] == '1') { 
+
+				//Set some session variables for easy reference
+				$_SESSION['name'] = $row['firstname'] . " " . $row['lastname'];
+				$_SESSION['id_user'] = $row['id_user'];
+
+				if(isset($_SESSION['callFrom'])) {
+					$location = $_SESSION['callFrom'];
+					unset($_SESSION['callFrom']);
+					
+					header("Location: " . $location);
+					exit();
+				} else {
+					header("Location: user/index.php");
+					exit();
+				}
+			} else if($row['active'] == '2') { 
+
+				$_SESSION['loginActiveError'] = "Your Account Is Deactivated. Contact Admin To Reactivate.";
+		 		header("Location: login.php");
+				exit();
+			}
+		}
+ 	}
+	else {
+
+ 		$_SESSION['loginError'] = $conn->error;
+ 		header("Location: login.php");
+		exit();
+ 	}
+	}
+	else if($value["role"] == "company")
+	{
+		$result = "";
+		if($conn->query($sql2)->num_rows > 0)
+		{
+		$result = $conn->query($company);
+		}
+		//if company table has this this login details
+		if($result->num_rows > 0) {
+			while($row = $result->fetch_assoc()) {
+				if($row['active'] == '2') {
+					$_SESSION['companyLoginError'] = "Your Account Is Still Pending Approval By Admin.";
+					header("Location: login.php");
+					exit();
+				} else if($row['active'] == '0') {
+					$_SESSION['companyLoginError'] = "Your Account Is Rejected. Please Contact Admin For More Info.";
+					header("Location: login.php");
+					exit();
+				} else if($row['active'] == '1') {
+					$_SESSION['name'] = $row['companyname'];
+					$_SESSION['id_company'] = $row['id_company'];
+
+					header("Location: company/index.php");
+					exit();
+				} else if($row['active'] == '3') {
+					$_SESSION['companyLoginError'] = "Your Account Is Deactivated. Contact Admin For Reactivation.";
+					header("Location: login.php");
+					exit();
+				}
+			}
+		}
+		 else {
+ 		$_SESSION['loginError'] = $conn->error;
+ 		header("Location: login.php");
+		exit();
+		}
+	}
+	else if($value["role"] == "admin")
+	{
+	$result = "";
+	if($conn->query($sql2)->num_rows > 0)
+	{
+	$result = $conn->query($sql2);
+	}
+	if($result->num_rows > 0) {
+		//output data
+		while($row = $result->fetch_assoc()) {
+			
+			$_SESSION['loginid'] = $row['loginid'];
+			header("Location: admin/index.php");
+			exit();
+		}
+ 	} else {
+ 		$_SESSION['loginError'] = true;
+ 		header("Location: admin/index.php");
+		exit();
+ 	}
+	}
+
+ 	$conn->close();
+
+} else {
+	header("Location: login.php");
+	exit();
+}
--- user/index.php
+++ user/index.php
@@ -1,3 +1,14 @@
+<?php
+
+session_start();
+
+if(empty($_SESSION['id_user'])) {
+  header("Location: ../index.php");
+  exit();
+}
+
+require_once("../db.php");
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -13,75 +24,72 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/ionicons/2.0.1/css/ionicons.min.css">
   <!-- Theme style -->
-  <link rel="stylesheet" href="css/AdminLTE.min.css">
-  <link rel="stylesheet" href="css/_all-skins.min.css">
+  <link rel="stylesheet" href="../css/AdminLTE.min.css">
+  <link rel="stylesheet" href="../css/_all-skins.min.css">
   <!-- Custom -->
-  <link rel="stylesheet" href="css/custom.css">
+  <link rel="stylesheet" href="../css/custom.css">
 
   <!-- Google Font -->
   <link rel="stylesheet"
         href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 </head>
-<body class="hold-transition skin-blue sidebar-mini">
+<body class="hold-transition skin-green sidebar-mini">
 <div class="wrapper">
 
   <header class="main-header">
 
     <!-- Logo -->
-    <a href="index.html" class="logo">
+    <a href="index.php" class="logo logo-bg">
       <!-- mini logo for sidebar mini 50x50 pixels -->
       <span class="logo-mini"><b>W</b>L</span>
       <!-- logo for regular state and mobile devices -->
       <span class="logo-lg"><b>Work</b>Lord</span>
     </a>
 
+    <!-- Header Navbar: style can be found in header.less -->
     <nav class="navbar navbar-static-top">
-      <div class="navbar-custom-menu">
-        <ul class="nav navbar-nav">
-          <li>
-            <a href="login.php">Login</a>
-          </li>
-          <li>
-            <a href="sign-up.php">Sign Up</a>
-          </li>    
-        </ul>
-      </div>
     </nav>
   </header>
 
+  <!-- Content Wrapper. Contains page content -->
   <div class="content-wrapper" style="margin-left: 0px;">
 
-    <section class="content-header bg-main">
+    <section id="candidates" class="content-header">
       <div class="container">
         <div class="row">
-          <div class="col-md-12 text-center index-head">
-            <h1>“If you can <strong>DREAM</strong> it, you can DO it ”</h1>
-            <p><a class="btn btn-success btn-lg" href="#" role="button">Search Jobs</a></p>
-          </div>
-        </div>
-      </div>
-    </section>
-
-    <section class="content-header">
-      <div class="container">
-        <div class="row">
-          <div class="col-md-12 latest-job margin-bottom-20">
-            <h1 class="text-center">Latest Jobs</h1>            
-            <div class="attachment-block clearfix">
+          <div class="col-md-3">
+            <div class="box box-solid">
+              <div class="box-header with-border">
+                <h3 class="box-title">Welcome <b><?php echo $_SESSION['name']; ?></b></h3>
+              </div>
+              <div class="box-body no-padding">
+                <ul class="nav nav-pills nav-stacked">
+                  <li><a href=""><i class="fa fa-user"></i> Edit Profile</a></li>
+                  <li><a href=""><i class="fa fa-address-card-o"></i> My Applications</a></li>
+                  <li><a href=""><i class="fa fa-list-ul"></i> Jobs</a></li>
+                  <li><a href=""><i class="fa fa-book"></i> Exams</a></li>
+                  <li><a href=""><i class="fa fa-envelope"></i> Mailbox</a></li>
+                  <li><a href=""><i class="fa fa-gear"></i> Settings</a></li>
+                  <li><a href="../logout.php"><i class="fa fa-arrow-circle-o-right"></i> Logout</a></li>
+                </ul>
+              </div>
             </div>
           </div>
         </div>
       </div>
     </section>
 
+    
+
   </div>
 
   <footer class="main-footer" style="margin-left: 0px;">
     <div class="text-center">
-      <strong>Copyright &copy; 2020 WordLord</a>.</strong> All rights
+      <strong>Copyright &copy; 2020 WorkLord.</strong> All rights
     reserved.
     </div>
   </footer>
+
 </div>
 
 <!-- jQuery 3 -->
@@ -89,6 +97,6 @@
 <!-- Bootstrap 3.3.7 -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- AdminLTE App -->
-<script src="js/adminlte.min.js"></script>
+<script src="../js/adminlte.min.js"></script>
 </body>
 </html>
