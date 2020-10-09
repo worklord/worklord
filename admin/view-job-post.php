ab1c4e8e53f794e4a5a35dd961fc1ae4584d9ab9
althafvly <althafvly@gmail.com>
2020-10-10 00:11:20 +0530

    worklord: add our logo

--- admin/view-job-post.php
+++ admin/view-job-post.php
@@ -27,6 +27,8 @@ if($result1->num_rows > 0)
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <title>WorkLord</title>
+    <!-- Favicons -->
+  <link rel="icon" href="img/logo.png">
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <!-- Bootstrap 3.3.7 -->
--- index.php
+++ index.php
@@ -16,7 +16,7 @@ require_once("db.php");
   <meta content="" name="keywords">
 
   <!-- Favicons -->
-  <link href="img/logo.png" rel="icon">
+  <link rel="icon" href="img/logo.png">
 
   <!-- Google Fonts -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
